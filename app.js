1ab0f1133197eb1cfcd940ba0c704294b9791aab
Eric Grosse <ericgrosse1@gmail.com>
2023-01-30 15:13:34 -0500

    Added a config.js file for tokens and api keys

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const WebSocket = require('ws');
+const config = require('./config');
 
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -16,9 +17,9 @@ server.listen(port, hostname, () => {
   const ws = new WebSocket('wss://irc-ws.chat.twitch.tv:443');
 
   ws.on('open', () => {
-    ws.send('PASS oauth:your_token');
-    ws.send('NICK your_username');
-    ws.send('JOIN #your_channel');
+    ws.send(`PASS oauth:${config.twitch.token}`);
+    ws.send(`NICK ${config.twitch.username}`);
+    ws.send(`JOIN #${config.twitch.channel}`);
   });
 
   ws.on('message', (data) => {
---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  twitch: {
+    token: 'your_token',
+    username: 'your_username',
+    channel: 'your_channel'
+  }
+};
