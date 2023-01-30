c976503def32a98f4159ab29e2e5b310ac98f489
Eric Grosse <ericgrosse1@gmail.com>
2023-01-30 15:10:32 -0500

    Added initial logic for implementing web sockets

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const WebSocket = require('ws');
 
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -10,5 +11,20 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`HTTP Server running at http://${hostname}:${port}/`);
+
+  const ws = new WebSocket('wss://irc-ws.chat.twitch.tv:443');
+
+  ws.on('open', () => {
+    ws.send('PASS oauth:your_token');
+    ws.send('NICK your_username');
+    ws.send('JOIN #your_channel');
+  });
+
+  ws.on('message', (data) => {
+    if (data.includes('PRIVMSG')) {
+      const message = data.split(':')[2];
+      console.log(`New message in Twitch chat: ${message}`);
+    }
+  });
 });
